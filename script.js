2f0651b78b4d50790089b26697babd9e4320fd21
elaine543 <elaine.almeida.silva@escola.pr.gov.br>
2025-09-16 09:32:31 -0300

    aula 4

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-
 const botaoMostraPalavras = document.querySelector( "#botao-paçavrachave");
 
 botaoMostraPalavras.addEventListener( " click", mostraPalavrasChave);
 
 function mostraPalavrasChave(){ 
-    const texto = document.querySelector( "#entrada-de-texto").ariaValueMax;
+const texto = document.querySelector( "#entrada-de-texto").ariaValueMax;
+const campoResultado = document. querySelector( "#resultado-palavrachave")
+const palavrasChave = processaTexto( texto);
 
-    const campoResultado = document. querySelector( "#resultado-palavrachave");
-
-    const palavras = texto.split("");
     campoResultado.textoContent = palavras; 
     
 } 
+ function ProcessaTexto (texto) {
+    let palavras = texto.split(/\P{L]+/u);
+    return palavras; 
+
+ } 
